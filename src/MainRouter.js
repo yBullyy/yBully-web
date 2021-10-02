8428fadfb8710d6df0317d1c68b22ba02d27fe30
shubh <shahshubh1010@gmail.com>
2021-10-02 19:36:37 +0530

    added Home page

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Admin from './admin/Admin';
+import Error from './error/Error';
 import Home from './home/Home';
+import Login from './login/Login';
 
 const MainRouter = () => (
     <div>
         <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/admin" component={Admin} />
+            <Route exact path="/login" component={Login} />
+            <Route component={Error} />
         </Switch>
     </div>
 );
